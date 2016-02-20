db7f9d8afde4b71283129c0a6bd90c89e88c2517
Dmitri Smirnov <dmitrism@microsoft.com>
2016-02-19 17:58:27 -0800

    Make sure that PDB files are named after the static lib   and delivered to the output directory.   Adjust build settings due to the following issue:   https://github.com/jemalloc/jemalloc/issues/332

--- CMake_configure.cmd
+++ CMake_configure.cmd
@@ -1,2 +1,2 @@
 REM Configure Jemalloc build with options
-CMake -G "Visual Studio 12 Win64" -Ddisable-fill=1 -Ddisable-stats=1  ..
+CMake -G "Visual Studio 12 Win64" -Ddisable-fill=1 -Ddisable-stats=1 -Ddisable-cache-oblivious=1 ..
