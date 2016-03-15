daadf32565b7c49a14dc48ec032b738d114fff02
Dmitri Smirnov <dmitrism@microsoft.com>
2016-03-15 15:52:30 -0700

    Use the latest experimental build settings.

--- CMake_configure.cmd
+++ CMake_configure.cmd
@@ -1,2 +1,5 @@
 REM Configure Jemalloc build with options
-CMake -G "Visual Studio 12 Win64" -Ddisable-fill=1 -Ddisable-stats=1 -Ddisable-cache-oblivious=1 ..
+CMake -G "Visual Studio 12 Win64" -Ddisable-fill=1  -Dwith-malloc-conf=purge:decay ..
+
+REM Debug build settings
+REM CMake -G "Visual Studio 12 Win64" -Denable-debug=1 -Dwith-malloc-conf=junk:true ..
