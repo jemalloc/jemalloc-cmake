2e7bff9b0156a2e891b047930caf1cbc3cdff49e
Dmitri Smirnov <dmitrism@microsoft.com>
2016-04-07 11:23:12 -0700

    Remove misleading comment causing some to configure Debug builds differently.   Those seettings were for debugging unit tests.

--- CMake_configure.cmd
+++ CMake_configure.cmd
@@ -1,5 +1,2 @@
 REM Configure Jemalloc build with options
 CMake -G "Visual Studio 12 Win64" -Ddisable-fill=1  -Dwith-malloc-conf=purge:decay ..
-
-REM Debug build settings
-REM CMake -G "Visual Studio 12 Win64" -Denable-debug=1 -Dwith-malloc-conf=junk:true ..
