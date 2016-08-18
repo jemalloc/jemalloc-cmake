a7e7f641c0fe6f633de4cc4add2c30df715eda55
Dmitri Smirnov <dmitrism@microsoft.com>
2016-08-18 11:53:44 -0700

    Enable debug options

--- CMake_configure14.cmd
+++ CMake_configure14.cmd
@@ -1,2 +1,2 @@
 REM Configure Jemalloc build with options
-CMake -G "Visual Studio 14 Win64" -Ddisable-fill=1  -Dwith-malloc-conf=purge:decay -Ddisable-tcache=1 ..
+CMake -G "Visual Studio 14 Win64" -Dwith-malloc-conf=purge:decay -Ddisable-tcache=1 -Denable-debug=1 ..
