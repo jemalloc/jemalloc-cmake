cdd92de1995234e752516abd5985539c4af10230
Jason Evans <jasone@canonware.com>
2016-10-13 14:47:50 -0700

    Bring spinning from e5effef428b5bf941e1697f6000c97f1ce734756

--- src/jemalloc.c
+++ src/jemalloc.c
@@ -1256,10 +1256,12 @@ malloc_init_hard_needed(void)
 	}
 #ifdef JEMALLOC_THREADED_INIT
 	if (malloc_initializer != NO_INITIALIZER && !IS_INITIALIZER) {
+     spin_t spinner;
 		/* Busy-wait until the initializing thread completes. */
+     spin_init(&spinner);
 		do {
 			malloc_mutex_unlock(NULL, &init_lock);
-			CPU_SPINWAIT;
+      spin_adaptive(&spinner);
 			malloc_mutex_lock(NULL, &init_lock);
 		} while (!malloc_initialized());
 		return (false);
